50560dee08fd6d865951da3fc3a2a9a300f50eca
Kaumini_Perera <kauminipathirage2000@gmail.com>
2023-10-15 17:12:02 +0530

    Initial commit

--- car.py
+++ car.py
@@ -1,8 +1,16 @@
+import random
 class Car:
-    name = ""
-    def __init__(self, brand):
+    def __init__(self, brand, model):
         self.brand = brand
-        self.mileage = 0
+        self.model = model
+        self.mileage = random.randint(1000, 50000)
 
     def describe(self):
-        return f"This is a {self.brand} car, with {self.mileage} miles on it."
+        return f"This is a {self.brand} {self.model} car, with {self.mileage} miles on it."
+
+brand_input = input("Enter your car brand here: ")
+model_input = input("Enter your car model here: ")
+
+my_car = Car(brand_input, model_input)
+print(my_car.describe())
+
