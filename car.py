0cc39ecc66bd8e6899247296e29cd1dc337d3ca5
Bhagya Karunarathna <karunarathnakbs.20@uom.lk>
2023-10-15 15:01:36 +0530

    Milage added to Car class

--- car.py
+++ car.py
@@ -2,3 +2,7 @@ class Car:
     name = ""
     def __init__(self, brand):
         self.brand = brand
+        self.mileage = 0
+
+        def describe(self):
+            return f"This is a {self.brand} car, with {self.mileage} miles on it."
